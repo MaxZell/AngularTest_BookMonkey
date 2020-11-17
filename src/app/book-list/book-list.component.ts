18f0eeb61986cd9d7afb199ecae48f1469eee2fd
mzelensky <max.m.zelensky@gmail.com>
2020-11-17 12:20:04 +0100

    lesson 1

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../shared/book';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  //https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc
+  books: Book[];
+
+  ngOnInit(): void {
+    this.books =[
+      {
+        isbn: '0001',
+        title: 'Die Abenteuer des Tom Sawyer',
+        authors: ['Mark Twain'],
+        published: new Date(1876, 1, 1),
+        thumbnails:[{
+          url: 'https://media.giphy.com/media/MKZxRoSup632N5HCHa/giphy.gif',
+          title: 'Buchcover'
+        }],
+      },
+      {
+        isbn: '0002',
+        title: 'Angular',
+        authors: ['Ferdinant Malcher', 'Johannes Hoppe','Danny Koppenhagen'],
+        published: new Date(2020, 8, 1),
+        subtitle: 'Grundlagen...',
+        rating: 5,
+        thumbnails:[{
+          url: 'https://ng-buch.de/angular-cover.jpg',
+          title: 'Buchcover'
+        }],
+      }
+    ];
+  }
+}
